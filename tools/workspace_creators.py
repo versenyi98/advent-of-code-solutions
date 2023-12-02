0e5d94106fcd8faed872ff2da9da2527be0b4317
Péter Versényi <versenyi98@gmail.com>
2023-12-02 21:08:41 +0100

    tools: fix info.json creation

--- tools/workspace_creators.py
+++ tools/workspace_creators.py
@@ -54,20 +54,20 @@ class AocWorkspaceCreator(WorkspaceCreator):
         super().__init__(url)
         self.problem_scraper = AoCProblemScraper(url)
         self.solutions_dir = self.base_path / "solutions"
-        self.problem_dir = None
+
+        self.problem_name = self.problem_scraper.get_problem_name()
+        self.problem_name = self.problem_name.replace("-", "").strip().replace(":", " -")
+        self.problem_name = make_windows_friendly(self.problem_name)
+        self.problem_dir = self.problem_name
 
     def create_directory(self):
         year_dir = self.url.split('/')[-3]
 
-        problem_dir = self.problem_scraper.get_problem_name()
-        problem_dir = problem_dir.replace("-", "").strip().replace(":", " -")
-        problem_dir = make_windows_friendly(problem_dir)
-
-        problem_dir_split = problem_dir.split()
+        problem_dir_split = self.problem_dir.split()
         if len(problem_dir_split[1]) == 1:
             problem_dir_split[1] = "0" + problem_dir_split[1]
-        problem_dir = " ".join(problem_dir_split)
-        self.problem_dir = self.solutions_dir / year_dir / problem_dir
+        self.problem_dir = " ".join(problem_dir_split)
+        self.problem_dir = self.solutions_dir / year_dir / self.problem_dir
 
         if not os.path.exists(self.problem_dir):
             os.makedirs(os.path.dirname(self.problem_dir), exist_ok=True)
@@ -77,14 +77,11 @@ class AocWorkspaceCreator(WorkspaceCreator):
         pass
 
     def write_info_json(self):
-        problem_name = self.problem_scraper.get_problem_name()
-        problem_name = problem_name.replace("-", "").split(":")[-1].strip()
-
         year = self.url.split('/')[-3]
         day = self.url.split('/')[-1]
 
         info = {
-            "Name": problem_name,
+            "Name": self.problem_name,
             "URL": self.url,
             "Year": year,
             "Day": day
